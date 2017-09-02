1007c18d958b9bfd9a0e46c1df9fd8ddad316165
Jovi <redzjovi@gmail.com>
2017-09-02 21:01:58 +0700

    Update my_helper.

--- application/helpers/my_helper.php
+++ application/helpers/my_helper.php
@@ -1,57 +1,50 @@
 <?php
 function datetime_from_format($datetime, $format_from, $format_to = 'Y-m-d H:i:s')
 {
-	$datetime_new = DateTime::createFromFormat($format_from, $datetime);
-	return $datetime_new->format($format_to);
+    $datetime_new = DateTime::createFromFormat($format_from, $datetime);
+    return $datetime_new->format($format_to);
 }
 
-function build_tree(Array $data, $parent = 0)
+function build_tree($elements = [], $parent_id = 0)
 {
-    $tree = array();
-    foreach ($data as $d)
-	{
-        if ($d['parent_id'] == $parent)
-		{
-            $children = build_tree($data, $d['id']);
-            // set a trivial key
-            if (!empty($children))
-			{
-                $d['child'] = $children;
+    $branch = array();
+
+    foreach ($elements as $element) {
+        if ($element['parent_id'] == $parent_id) {
+            $children = build_tree($elements, $element['id']);
+            if ($children)  {
+                $element['child'] = $children;
             }
-            $tree[] = $d;
+            $branch[] = $element;
         }
     }
-	return $tree;
+
+    return $branch;
 }
 
 function pr($data)
 {
-	if (is_array($data))
-	{
-		echo '<pre>';
-		print_r($data);
-		echo '</pre>';
-	}
-	else
-		var_dump($data);
+    if (is_array($data))
+    {
+        echo '<pre>';
+        print_r($data);
+        echo '</pre>';
+    }
+    else
+    {
+        var_dump($data);
+    }
 }
 
-function print_tree($tree, $r = 0, $p = null, $data = array())
+function print_tree($elements = [], $prefix = '-', $parent_id = 0, $branch = [])
 {
-	foreach ($tree as $i => $t)
-	{
-        $dash = ($t['parent_id'] == 0) ? '' : str_repeat('-', $r).' ';
-        $data[$t['id']] = $dash.$t['text'];
-        if ($t['parent_id'] == $p)
-		{
-            // reset $r
-            $r = 0;
-        }
-        if (isset($t['child']))
-		{
-            $data = print_tree($t['child'], ++$r, $t['parent_id'], $data);
+    foreach ($elements as $element) {
+        $tree_prefix = ($element['parent_id'] == 0) ? '' : $prefix;
+        $branch[$element['id']] = $tree_prefix.$element['text'];
+        if (isset($element['child'])) {
+            $branch = print_tree($element['child'], $tree_prefix.$prefix, $element['parent_id'], $branch);
         }
     }
-	return $data;
+
+    return $branch;
 }
-?>

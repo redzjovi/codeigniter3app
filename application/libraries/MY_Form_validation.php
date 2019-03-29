e73d5c7355a7a5d35b011e1dc84dbdf34c6c0593
Jovi <redzjovi@gmail.com>
2019-03-30 04:24:39 +0700

    [FormValidation] add not_exist rule.

--- application/libraries/MY_Form_validation.php
+++ application/libraries/MY_Form_validation.php
@@ -2,23 +2,45 @@
 
 class MY_Form_validation extends CI_Form_validation
 {
-    protected $ci;
+    protected $CI;
 
-    function __construct()
+    public function __construct()
     {
         parent::__construct();
-        $this->ci =& get_instance();
+        $this->CI =& get_instance();
+    }
+
+    /**
+     * @param string $valuw
+     * @param string $tableAndColumn Ie. "domain,url"
+     * @return bool
+     */
+    public function not_exists($value, $tableAndColumn)
+    {
+        $tableAndColumnArray = explode(',', $tableAndColumn);
+        $table = $tableAndColumnArray[0];
+        $column = $tableAndColumnArray[1];
+
+        $this->CI->load->database();
+        $count = $this->CI->db->from($table)->where($column, $value)->count_all_results();
+        if ($count === 0)
+        {
+            $this->CI->form_validation->set_message('not_exists', 'The %s field does not exist in '.$table.'.');
+            return FALSE;
+        }
+
+        return TRUE;
     }
 
     public function recaptcha_check()
     {
-        $this->ci->load->library('recaptcha');
+        $this->CI->load->library('recaptcha');
 
         // Catch the user's answer
-        $captcha_answer = $this->ci->input->post('g-recaptcha-response');
+        $captcha_answer = $this->CI->input->post('g-recaptcha-response');
 
         // Verify user's answer
-        $response = $this->ci->recaptcha->verifyResponse($captcha_answer);
+        $response = $this->CI->recaptcha->verifyResponse($captcha_answer);
 
         if ($response['success']) // Your success code here
         {
@@ -26,7 +48,7 @@ class MY_Form_validation extends CI_Form_validation
         }
         else // Something goes wrong
         {
-            $this->ci->form_validation->set_message('recaptcha_check', $response);
+            $this->CI->form_validation->set_message('recaptcha_check', $response);
             return FALSE;
         }
     }
